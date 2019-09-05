e1080c12caad12b6bbb59684eb75accbc446e942
tania <tanislavens@yahoo.com>
2019-09-05 12:45:34 -0400

    added a require to index

--- api/server.js
+++ api/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 
 // prints all node environment details specific to your comp
-console.log('environment', process.env);
-// prints undefined here but shows up in Heroku
+// console.log('environment', process.env);
+// if prints undefined here should show up in Heroku
 console.log('environment', process.env.NODE_ENV);
 
 const apiRouter = require('./api-router.js');
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const server = require('./api/server.js');
 
+require('dotenv').config();
+
 const port = 4000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
