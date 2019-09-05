55af8ae23aa7dd6c5b0ea730533c329f6818fbe2
tania <tanislavens@yahoo.com>
2019-09-05 12:44:06 -0400

    installed dotenv and made a .env file

--- api/server.js
+++ api/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const helmet = require('helmet');
 
+// prints all node environment details specific to your comp
+console.log('environment', process.env);
+// prints undefined here but shows up in Heroku
+console.log('environment', process.env.NODE_ENV);
+
 const apiRouter = require('./api-router.js');
 
 const server = express();
