d03efe56adf87264e6fcf8fc253ea8943062e496
tania <tanislavens@yahoo.com>
2019-09-19 13:57:52 -0400

    adds postgres

--- config/secrets.js
+++ config/secrets.js
@@ -1,3 +1,4 @@
 module.exports = {
     googleApiKey: process.env.GOOGLE_API_KEY || 'fake google api key',
+    environment: process.env.NODE_ENV || 'development'
 };
--- knexfile.js
+++ knexfile.js
@@ -18,16 +18,8 @@ module.exports = {
     },
   },
   production: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: './data/shouts.db3',
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      },
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations',
     },
